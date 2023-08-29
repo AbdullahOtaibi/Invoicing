5b28143efb94c984fd1af47e165d0eb0ef6ef619
Osama <osama.kofahi@gmail.com>
2023-08-29 17:11:48 +0300

    up

--- nodeJS/control-panel/src/components/admin/Layout/Sidebar.js
+++ nodeJS/control-panel/src/components/admin/Layout/Sidebar.js
@@ -121,12 +121,7 @@ const Sidebar = () => {
                     </li>) : null}
 
 
-                {localStorage.getItem("permissions") != null && localStorage.getItem("permissions").indexOf("invoices.view") > -1 ? (
-                    <li className={getClass('/admin/Receipt')}>
-                        <Link className="wave-effect waves-effect waves-button" to="/admin/Receipt" >
-                            <MdReceipt size={18} /> &nbsp; {t("sidebar.Receipt")}
-                        </Link >
-                    </li>) : null}
+               
 
                 {localStorage.getItem("permissions") != null && localStorage.getItem("permissions").indexOf("invoices.view") > -1 ? (
                     <li className={getClass('/admin/invoices')}>
