c571d6c5538b7fa315210790b228e486b32cb4b6
Osama <osama.kofahi@gmail.com>
2023-10-14 16:08:03 +0300

    cleanup

--- nodeJS/control-panel/src/components/admin/Layout/MainContent.js
+++ nodeJS/control-panel/src/components/admin/Layout/MainContent.js
@@ -13,47 +13,21 @@ import CompanyCategories from '../Companies/CompanyCategories'
 const ListModules = lazy(() => import(/* webpackChunkName: "ListModules" */'../Modules/ListModules'))
 const CreateModule = lazy(() => import(/* webpackChunkName: "CreateModule" */'../Modules/CreateModule'))
 
-const ListArticles = lazy(() => import(/* webpackChunkName: "ListArticles" */ '../Articles/ListArticles'))
-const EditArticle = lazy(() => import(/* webpackChunkName: "EditArticle" */'../Articles/EditArticle'))
-const CreateArticle = lazy(() => import(/* webpackChunkName: "CreateArticle" */'../Articles/CreateArticle'))
+
 const ListCategories = lazy(() => import(/* webpackChunkName: "ListCategories" */'../Categories/ListCategories'))
 const ListCounters = lazy(() => import(/* webpackChunkName: "ListCounters" */'../Counters/ListCounters'))
-const Gallery = lazy(() => import(/* webpackChunkName: "Gallery" */'../Gallery/Gallery'))
-const ListNews = lazy(() => import(/* webpackChunkName: "ListNews" */'../News/ListNews'))
 const ListPages = lazy(() => import(/* webpackChunkName: "ListPages" */'../Pages/ListPages'))
-const ListPartners = lazy(() => import(/* webpackChunkName: "ListPartners" */'../Partners/ListPartners'))
-const ListProjects = lazy(() => import(/* webpackChunkName: "ListProjects" */'../Projects/ListProjects'))
 const ListSettings = lazy(() => import(/* webpackChunkName: "ListSettings" */'../Settings/ListSettings'))
-const ListTeams = lazy(() => import(/* webpackChunkName: "ListTeams" */'../Team/ListTeams'))
-const ListTestimonials = lazy(() => import(/* webpackChunkName: "ListTestimonials" */'../Testimonials/ListTestimonials'))
 const ListMenus = lazy(() => import(/* webpackChunkName: "ListMenus" */'../Menus/ListMenus'))
 const CreateMenu = lazy(() => import(/* webpackChunkName: "CreateMenu" */'../Menus/CreateMenu'))
 const EditMenu = lazy(() => import(/* webpackChunkName: "EditMenu" */'../Menus/EditMenu'))
 const CreateCategory = lazy(() => import(/* webpackChunkName: "CreateCategory" */'../Categories/CreateCategory'))
 const EditCategory = lazy(() => import(/* webpackChunkName: "EditCategory" */'../Categories/EditCategory'))
-const CreateProject = lazy(() => import(/* webpackChunkName: "CreateProject" */'../Projects/CreateProject'))
-const EditProject = lazy(() => import(/* webpackChunkName: "EditProject" */'../Projects/EditProject'))
-const CreatePartner = lazy(() => import(/* webpackChunkName: "CreatePartner" */'../Partners/CreatePartner'))
-const EditPartner = lazy(() => import(/* webpackChunkName: "EditPartner" */'../Partners/EditPartner'))
-const ListSlides = lazy(() => import(/* webpackChunkName: "ListSlides" */'../Slider/ListSlides'))
-const CreateSlide = lazy(() => import(/* webpackChunkName: "CreateSlide" */'../Slider/CreateSlide'))
-const EditSlide = lazy(() => import(/* webpackChunkName: "EditSlide" */'../Slider/EditSlide'))
-const CreateGalleryItem = lazy(() => import(/* webpackChunkName: "CreateGalleryItem" */'../Gallery/CreateGalleryItem'))
-const EditGalleyItem = lazy(() => import(/* webpackChunkName: "EditGalleryItem" */'../Gallery/EditGalleryItem'))
 
-const CreateTestimonial = lazy(() => import(/* webpackChunkName: "CreateTestimonial" */'../Testimonials/CreateTestimonial'))
-const EditTestimonial = lazy(() => import(/* webpackChunkName: "EditTestimonial" */'../Testimonials/EditTestimonial'))
 const EditMenuItem = lazy(() => import(/* webpackChunkName: "EditMenuItem" */'../Menus/EditMenuItem'))
 
-const ListProductCategories = lazy(() => import(/* webpackChunkName: "ListProductCategories" */'../ProductCategories/ListProductCategories'))
-const CreateProductCategory = lazy(() => import(/* webpackChunkName: "CreateProductCategory" */'../ProductCategories/CreateProductCategory'))
-const EditProductCategory = lazy(() => import(/* webpackChunkName: "EditProductCategory" */'../ProductCategories/EditProductCategory'))
 
-const ShowProductCategories = lazy(() => import(/* webpackChunkName: "ShowProductCategories" */'../ProductCategories/ShowProductCategories'))
 
-const ListProducts = lazy(() => import(/* webpackChunkName: "ListProducts" */'../Products/ListProducts'))
-const CreateProduct = lazy(() => import(/* webpackChunkName: "CreateProduct" */'../Products/CreateProduct'))
-const EditProduct = lazy(() => import(/* webpackChunkName: "EditProduct" */'../Products/EditProduct'))
 
 const EditUser = lazy(() => import(/* webpackChunkName: "EditUser" */'../Users/EditUser'))
 const CreateUser = lazy(() => import(/* webpackChunkName: "CreateUser" */'../Users/CreateUser'))
@@ -74,20 +48,9 @@ const EditCompany = lazy(() => import(/* webpackChunkName: "EditCompany" */'../C
 
 const ListCountries = lazy(() => import(/* webpackChunkName: "ListCountries" */'../Countries/ListCountries'))
 const CreateCountry = lazy(() => import(/* webpackChunkName: "CreateCountry" */'../Countries/CreateCountry'))
-const ListOrders = lazy(() => import(/* webpackChunkName: "ListOrders" */'../Orders/ListOrders'))
-const ViewOrder = lazy(() => import(/* webpackChunkName: "ViewOrder" */'../Orders/ViewOrder'))
-const EditOrder = lazy(() => import(/* webpackChunkName: "EditOrder" */'../Orders/EditOrder'))
-const ViewQuotation = lazy(() => import(/* webpackChunkName: "ViewQuotation" */'../Quotations/ViewQuotation'))
-const ListQuotations = lazy(() => import(/* webpackChunkName: "ListQuotations" */'../Quotations/ListQuotations'))
-const EditQuotation = lazy(() => import(/* webpackChunkName: "EditQuotation" */'../Quotations/EditQuotation'))
 
-const ListShippingCompanies = lazy(() => import(/* webpackChunkName: "ListShippingCompanies" */'../ShippingCompanies/ListShippingCompanies'))
-const CreateShippingCompany = lazy(() => import(/* webpackChunkName: "CreateShippingCompany" */'../ShippingCompanies/CreateShippingCompany'))
-const EditShippingCompany = lazy(() => import(/* webpackChunkName: "EditShippingCompany" */'../ShippingCompanies/EditShippingCompany'))
 
-const ViewShipment = lazy(() => import(/* webpackChunkName: "ViewShipment" */'../Shipments/ViewShipment'))
-const ListShipments = lazy(() => import(/* webpackChunkName: "ListShipments" */'../Shipments/ListShipments'))
-const EditShipment = lazy(() => import(/* webpackChunkName: "EditShipment" */'../Shipments/EditShipment'))
+
 
 const ListNavigationMenus = lazy(() => import(/* webpackChunkName: "ListNavigationMenus" */'../NavigationMenus/ListNavigationMenus'))
 const CreateNavigationMenu = lazy(() => import(/* webpackChunkName: "CreateNavigationMenu" */'../NavigationMenus/CreateNavigationMenu'))
@@ -184,11 +147,7 @@ const MainContent = ({notification, onHandleNotification}) => {
                     <Route path="/forgot-password" exact element={<ForgotPassword />} />
                     <Route path="/reset-password/:token"  element={<ResetPassword />} />
                     
-                    <Route path="/articles" exact element={<ListArticles />} />
-                    <Route path="/orders" exact element={<ListOrders notification={notification} onHandleNotification={onHandleNotification}  />} />
-                    <Route path="/orders/:orderId" element={<ViewOrder notification={notification} onHandleNotification={onHandleNotification} />}  />
-                    <Route path="/orders/edit/:orderId" element={<EditOrder notification={notification} onHandleNotification={onHandleNotification} />}  />
-
+                  
                     <Route path="/translations" exact element={<ListTranslations />} />
 
                     <Route path="/translations/edit/:id" exact element={<EditTranslation />} />
@@ -199,13 +158,6 @@ const MainContent = ({notification, onHandleNotification}) => {
                     <Route path="/outstanding-payments" exact element={<OutstandingPayments />} />
 
 
-                    <Route path="/quotations" exact element={<ListQuotations />} />
-                    <Route path="/quotations/:quotationId" element={<ViewQuotation />} />
-                    <Route path="/quotations/edit/:quotationId" element={<EditQuotation />} />
-
-
-                    <Route path="/articles/edit/:id" element={<EditArticle />} />
-                    <Route path="/articles/create" element={<CreateArticle />} />
                     <Route path="/pages" element={<ListPages />} />
                     <Route path="/settings" element={<ListSettings />} />
                     <Route path="/communications" element={<ListMailTemplates />} />
@@ -217,28 +169,14 @@ const MainContent = ({notification, onHandleNotification}) => {
 
                     <Route path="/communications/create-mail-template" element={<CreateMailTemplate />} />
                     <Route path="/communications/edit-mail-template/:id" element={<EditMailTemplate />} />
-                    <Route path="/projects/create" element={<CreateProject />} />
-                    <Route path="/projects/edit/:id" element={<EditProject />} />
-                    <Route path="/projects" exact element={<ListProjects />} />
-                    <Route path="/news" element={<ListNews />} />
-                    <Route path="/gallery/create" element={<CreateGalleryItem />} />
-                    <Route path="/gallery/edit/:id" element={<EditGalleyItem />} />
-                    <Route path="/gallery" exact element={<Gallery />} />
-                    <Route path="/slider/create" element={<CreateSlide />} />
-                    <Route path="/slider/edit/:slideId" element={<EditSlide />} />
-                    <Route path="/slider" exact element={<ListSlides />} />
+
+
                     <Route path="/categories" exact element={<ListCategories />} />
                     <Route path="/categories/create" element={<CreateCategory />} />
                     <Route path="/categories/edit/:id" element={<EditCategory />} />
 
 
-                    <Route path="/testimonials/create" element={<CreateTestimonial />} />
-                    <Route path="/testimonials/edit/:id" element={<EditTestimonial />} />
-                    <Route path="/testimonials" exact element={<ListTestimonials />} />
-                    <Route path="/team" element={<ListTeams />} />
-                    <Route path="/partners/create" element={<CreatePartner />} />
-                    <Route path="/partners/edit/:id" element={<EditPartner />} />
-                    <Route path="/partners" exact element={<ListPartners />} />
+
                     <Route path="/counters" element={<ListCounters />} />
                    
                     <Route path="/menus/create" element={<CreateMenu />} />
@@ -256,21 +194,10 @@ const MainContent = ({notification, onHandleNotification}) => {
                     <Route path="/countries" exact element={<ListCountries />} />
                     <Route path="/countries/create" element={<CreateCountry />} />
 
-                    <Route path="/product-categories/create" element={<CreateProductCategory />} />
-                    <Route path="/product-categories/show" element={<ShowProductCategories />} />
+
                     
-                    <Route path="/product-categories/edit/:categoryId" element={<EditProductCategory />} />
-                    <Route path="/product-categories" exact element={<ListProductCategories />} />
-
-                    <Route path="/products" exact element={<ListProducts />} />
 
 
-                    <Route path="/products/bycompany/:companyIdParam" exact element={<ListProducts />} />
-                    <Route path="/products/byCategory/:categoryIdParam" exact element={<ListProducts />} />
-                    <Route path="/products/create" element={<CreateProduct />} />
-                    <Route path="/products/edit/:productId" exact element={<EditProduct />} />
-
-                   
 
 
                     <Route path="/clients" exact element={<ListClients />} />
@@ -338,13 +265,7 @@ const MainContent = ({notification, onHandleNotification}) => {
                     <Route path="/expenseCategories/view/:expenseCategoryId" element={<ViewExpensesCategory />} />
                     <Route path="/expenseCategories/edit/:expenseCategoryId" element={<EditExpensesCategory/>} />
 
-                    <Route path="/shippingCompanies" exact element={<ListShippingCompanies />} />
-                    <Route path="/shippingCompanies/create" element={<CreateShippingCompany />} />
-                    <Route path="/shippingCompanies/edit/:companyId" element={<EditShippingCompany />} />
 
-                    <Route path="/shipments/:shipmentId" element={<ViewShipment />} />
-                    <Route path="/shipments/edit/:shipmentId" element={<EditShipment />} />
-                    <Route path="/shipments" exact element={<ListShipments />} />
 
 
                     <Route path="" exact element={<Summary notification={notification} onHandleNotification={onHandleNotification} />} />
